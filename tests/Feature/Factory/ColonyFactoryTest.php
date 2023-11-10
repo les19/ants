4fafa765a2c92422b596c0447e9174d9e4633f02
les19 <les19@users.noreply.github.com>
2023-11-10 10:43:23 +0000

    Update code style.

--- tests/Feature/Factory/ColonyFactoryTest.php
+++ tests/Feature/Factory/ColonyFactoryTest.php
@@ -4,8 +4,6 @@ namespace Tests\Feature\Factory;
 
 use Database\Factories\ColonyFactory;
 use Illuminate\Database\QueryException;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ColonyFactoryTest extends TestCase
